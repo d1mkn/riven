701a3936ef33599c42e6adc80303ba8df3acbcdf
Dmytro Hubin <dimkanishe2099@gmail.com>
2024-05-11 03:04:45 +0300

    benefits block

---
+++ wp-content/themes/riven/gu-blocks/acf-block-benefits.php
@@ -0,0 +1,38 @@
+<?php
+
+$benefits_fields = get_fields();
+
+if (!empty($benefits_fields)) :
+    $title_benefits = $benefits_fields['title_benefits'] ?? '';
+    $subtitle_benefits = $benefits_fields['subtitle_benefits'] ?? '';
+    $items_benefits = $benefits_fields['items_benefits'] ?? []; ?>
+
+    <section class="benefits container">
+        <?php
+        print_elem('h2', 'benefits__title h1-t', $title_benefits);
+        print_elem('p', 'benefits__subtitle s1-t', $subtitle_benefits);
+
+        if (!empty($items_benefits)) : ?>
+            <ul class="benefits__list">
+                <?php
+                foreach ($items_benefits as $i => $item) :
+                    echo '<li class="benefits__item">';
+                    print_elem('figure', 'benefits__item-icon', wp_get_attachment_image(
+                        $item['icon_benefits'],
+                        'full',
+                        false,
+                        array(
+                            'alt' => $item['icon_alt_benefits'],
+                            'loading' => 'lazy',
+                            'decoding' => 'async',
+                        )
+                    ));
+                    print_elem('p', 'benefits__item-descr h2-t', $item['descr_benefits']);
+                    echo '</li>';
+                endforeach; ?>
+            </ul>
+        <?php
+        endif; ?>
+    </section>
+<?php
+endif; ?>
--- wp-content/themes/riven/gu-blocks/acf_blocks_enable.php
+++ wp-content/themes/riven/gu-blocks/acf_blocks_enable.php
@@ -21,8 +21,6 @@ if (!function_exists('promo')) {
                     if (!is_admin()) :
                         wp_enqueue_style('promo-css', get_template_directory_uri() . '/dist/css/gu-blocks/acf-block-promo.css',  ['popup-css'], _S_VERSION);
 
-                        // wp_enqueue_script('promo-js', get_template_directory_uri() . '/dist/js/gu-blocks/acf-block-promo.js', [], _S_VERSION, true);
-
                         wp_enqueue_style('popup-css', get_template_directory_uri() . '/dist/css/components/separate/popup.css', [], _S_VERSION);
                         wp_enqueue_style('form-css', get_template_directory_uri() . '/dist/css/components/separate/form.css', [], _S_VERSION);
                     endif;
@@ -34,6 +32,39 @@ if (!function_exists('promo')) {
 
 if (function_exists('promo')) {
     add_action('acf/init', 'promo');
+}
+
+// ###############################################################################################
+
+/**
+ * ACF Gutenberg Block "ACF Block Benefits"
+ */
+if (!function_exists('benefits')) {
+
+    function benefits()
+    {
+
+        acf_register_block_type(
+            [
+                'name'            => 'Benefits',
+                'title'           => __('Benefits'),
+                'render_callback' => 'theme_acf_blocks_render_callback',
+                'category'        => 'common',
+                'icon'            => 'editor-insertmore',
+                'keywords'        => ['title', 'subtitle','benefits'],
+                'mode'            => 'edit',
+                'enqueue_assets' => function () {
+                    if (!is_admin()) :
+                        wp_enqueue_style('benefits-css', get_template_directory_uri() . '/dist/css/gu-blocks/acf-block-benefits.css',  [], _S_VERSION);
+                    endif;
+                }
+            ]
+        );
+    }
+}
+
+if (function_exists('benefits')) {
+    add_action('acf/init', 'benefits');
 }
 
 // ###############################################################################################
